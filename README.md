5d6d7583402e5d8dffe82192d953bc2775f2503c
Matt Layher <mdlayher@gmail.com>
2020-05-28 20:27:17 -0400

    cmd/keylight: allow updating devices
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- README.md
+++ README.md
@@ -16,9 +16,27 @@ At the moment, the only supported operation is toggling the light state for
 a device. The default device address is `http://keylight:9123` which you can
 set up as a DNS name or similar for ease of use.
 
+With no arguments, the device is toggled on and off:
+
 ```
-$ keylight
-device "DEADBEEF9999", light 0 on
-$ keylight
-device "DEADBEEF9999", light 0 off
+$ keylight 
+device "keylight", light 0 on: temperature 4200K, brightness 20%
+$ keylight 
+device "keylight", light 0 off
+```
+
+You can also query the device's status or modify its parameters using other flags:
+
+```
+$ keylight -h
+Usage of keylight:
+  -a string
+        the address of an Elgato Key Light's HTTP API (default "http://keylight:9123")
+  -b int
+        set the brightness of a light to the specified percentage (valid: 3-100 %)
+  -d string
+        set the display name of an Elgato Key Light device
+  -i    display the current status of an Elgato Key Light without changing its state
+  -t int
+        set the color temperature of a light to the specified value (valid: 2900-7000 K)
 ```
--- cmd/keylight/main.go
+++ cmd/keylight/main.go
@@ -15,17 +15,30 @@ import (
 func main() {
 	log.SetFlags(0)
 
-	addr := flag.String("addr", "http://keylight:9123", "the address of an Elgato Key Light's HTTP API")
+	var (
+		addr        = flag.String("a", "http://keylight:9123", "the address of an Elgato Key Light's HTTP API")
+		brightness  = flag.Int("b", 0, "set the brightness of a light to the specified percentage (valid: 3-100 %)")
+		display     = flag.String("d", "", "set the display name of an Elgato Key Light device")
+		info        = flag.Bool("i", false, "display the current status of an Elgato Key Light without changing its state")
+		temperature = flag.Int("t", 0, "set the color temperature of a light to the specified value (valid: 2900-7000 K)")
+	)
 	flag.Parse()
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// TODO: build out with more functionality than toggling the light.
-
 	c, err := keylight.NewClient(*addr, nil)
 	if err != nil {
 		log.Fatalf("failed to create Key Light client: %v", err)
+	}
+
+	if *display != "" {
+		// Set the device's display name and then force info display to show
+		// the updated values.
+		if err := c.SetDisplayName(ctx, *display); err != nil {
+			log.Fatalf("failed to set display name: %v", err)
+		}
+		*info = true
 	}
 
 	d, err := c.AccessoryInfo(ctx)
@@ -38,15 +51,46 @@ func main() {
 		log.Fatalf("failed to fetch lights: %v", err)
 	}
 
-	// Toggle the status of each light and report on it.
-	for i, l := range lights {
-		l.On = !l.On
+	if *info {
+		// Log info and don't modify any settings.
+		logInfo(d, lights)
+		return
+	}
 
-		name := d.DisplayName
-		if name == "" {
-			name = d.SerialNumber
+	// Only toggle the light if no modification flags are set.
+	toggle := *brightness == 0 && *temperature == 0
+
+	for _, l := range lights {
+		if *brightness != 0 {
+			l.Brightness = *brightness
+		}
+		if *temperature != 0 {
+			l.Temperature = *temperature
 		}
 
+		if toggle {
+			l.On = !l.On
+		} else {
+			// If the light is being modified, force it on.
+			l.On = true
+		}
+	}
+
+	logInfo(d, lights)
+
+	if err := c.SetLights(ctx, lights); err != nil {
+		log.Fatalf("failed to set lights: %v", err)
+	}
+}
+
+// logInfo logs information about a device and its lights.
+func logInfo(d *keylight.Device, ls []*keylight.Light) {
+	name := d.DisplayName
+	if name == "" {
+		name = d.SerialNumber
+	}
+
+	for i, l := range ls {
 		onOff := "off"
 		if l.On {
 			onOff = fmt.Sprintf("on: temperature %dK, brightness %d%%",
@@ -54,9 +98,5 @@ func main() {
 		}
 
 		log.Printf("device %q, light %d %s", name, i, onOff)
-	}
-
-	if err := c.SetLights(ctx, lights); err != nil {
-		log.Fatalf("failed to set lights: %v", err)
 	}
 }
